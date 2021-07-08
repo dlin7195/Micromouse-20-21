449a6beac607417d4004f3e17bfee9eced948269
dlin7195 <70040558+dlin7195@users.noreply.github.com>
2021-07-08 13:00:13 -0700

    Update pid.c

--- pid.c
+++ pid.c
@@ -1,7 +1,7 @@
 /*
  * pid.c
  *
- *  Created on: Sep 27, 2020
+ *  Template created on: Sep 27, 2020
  *      Author: Tyler Price
  */
 
@@ -34,13 +34,10 @@ int encoderCount_avg = 0;
 void resetPID()
 {
 	/*
-	 * For assignment 3.1: This function does not need to do anything
-	 * For assignment 3.2: This function should reset all the variables you define in this file to help with PID to their default
-	 *  values. You should also reset your motors and encoder counts (if you tell your rat to turn 90 degrees, there will be a big
-	 * difference in encoder counts after it turns. If you follow that by telling your rat to drive straight without first
-	 * resetting the encoder counts, your rat is going to see a huge angle error and be very unhappy).
+	 * Rreset all the PID-assisting variables defined in this file to default values.
+	 * Reset your motors and encoder counts.
 	 *
-	 * You should additionally set your distance and error goal values (and your oldDistanceError and oldAngleError) to zero.
+	 * Reset distance and error goal values (+ oldDistanceError and oldAngleError) to zero.
 	 */
 	angleError = 0;
 	distanceError = 0;
@@ -67,21 +64,10 @@ void resetPID()
 void updatePID()
 {
 	/*
-	 * This function will do the heavy lifting PID logic. It should do the following: read the encoder counts to determine an error,
-	 * use that error along with some PD constants you determine in order to determine how to set the motor speeds, and then actually
-	 * set the motor speeds.
-	 *
-	 * For assignment 3.1: implement this function to get your rat to drive forwards indefinitely in a straight line. Refer to pseudocode
-	 * example document on the google drive for some pointers
-	 *
-	 * TIPS (assignment 3.1): Create kPw and kDw variables, use a variable to store the previous error for use in computing your
-	 * derivative term. You may get better performance by having your kDw term average the previous handful of error values instead of the
-	 * immediately previous one, or using a stored error from 10-15 cycles ago (stored in an array?). This is because systick calls so frequently
-	 * that the error change may be very small and hard to operate on.
-	 *
-	 * For assignment 3.2: implement this function so it calculates distanceError as the difference between your goal distance and the average of
-	 * your left and right encoder counts. Calculate angleError as the difference between your goal angle and the difference between your left and
-	 * right encoder counts. Refer to pseudocode example document on the google drive for some pointers.
+	 * PID logic. It should do the following: 
+	 * read the encoder counts to determine an error
+	 * use that error along with some PD constants in order to determine how to set the motor speeds
+	 * then actually set the motor speeds.
 	 */
 
      angleError = targetAngle + (((int16_t) TIM2->CNT) - ((int16_t) TIM8->CNT));
@@ -136,8 +122,7 @@ void updatePID()
 void setPIDGoalD(int16_t distance)
 {
 	/*
-	 * For assignment 3.1: this function does not need to do anything.
-	 * For assignment 3.2: this function should set a variable that stores the goal distance.
+	 * set variable that stores the goal distance.
 	 */
 	targetDistance = distance;
 }
@@ -145,8 +130,7 @@ void setPIDGoalD(int16_t distance)
 void setPIDGoalA(int16_t angle)
 {
 	/*
-	 * For assignment 3.1: this function does not need to do anything
-	 * For assignment 3.2: This function should set a variable that stores the goal angle.
+	 * set variable that stores the goal angle.
 	 */
 	targetAngle = angle;
 }
@@ -154,10 +138,9 @@ void setPIDGoalA(int16_t angle)
 int8_t PIDdone(void)
 {
 	/*
-	 * For assignment 3.1: this function does not need to do anything (your rat should just drive straight indefinitely)
-	 * For assignment 3.2:This function should return true if the rat has achieved the set goal. One way to do this by having updatePID() set some variable when
-	 * the error is zero (realistically, have it set the variable when the error is close to zero, not just exactly zero). You will have better results if you make
-	 * PIDdone() return true only if the error has been sufficiently close to zero for a certain number, say, 50, of SysTick calls in a row.
+	 * return true if the rat has achieved the set goal. 
+	 * return true only if the error has been sufficiently close to zero for a certain number of SysTick calls in a row.
+	 * (will realistically never be actually 0)
 	 */
     if (isAngleDone == 1 && isDistanceDone == 1)
     {
