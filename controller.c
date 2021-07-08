7c04103f0f478b686f673cf362876da131cba181
dlin7195 <70040558+dlin7195@users.noreply.github.com>
2021-07-08 12:47:51 -0700

    Update controller.c

--- controller.c
+++ controller.c
@@ -1,6 +1,6 @@
 /*
  * controller.c
- *
+ *  Template
  *  Created on: Sep 27, 2020
  *      Author: Tyler Price
  */
@@ -12,21 +12,11 @@
 int right_angle = 602; //595
 int one_cell = 625; //615
 int isPIDdone = 0;
-/*
- * We recommend you implement this function so that move(1) will move your rat 1 cell forward.
- */
+
+// move 1 cell
 void move(int8_t n)
 {
 	//resetPID();
-	/*
-	 * For assignment 3.1: Don't worry about implementing this yet
-	 * For assignment 3.2: This function should set the distance and angle goals appropriately for PID (hint: using the setGoal functions in pid.c)
-	 * and wait until the error becomes sufficiently small and persistent before exiting. This function should NOT exit before then.
-	 *
-	 * HINT: Use a while loop to wait until the rat has moved the desired distance
-	 *
-	 * You should also call resetPID before exiting this function so your rat is ready for the next instruction.
-	 */
 	setPIDGoalA(0);  // straight line movement
 	setPIDGoalD(n*one_cell);
 	while (1)
@@ -43,22 +33,10 @@ void move(int8_t n)
 	return;
 }
 
-/*
- * We recommend you implement this function so that turn(1) turns your rat 90 degrees in your positive rotation
- * direction and turn(-1) turns the other way.
- */
+// turn 90 degrees right
 void turn(int8_t n)
 {
 	//resetPID();
-	/*
-	 * For assignment 3.1: Don't worry about implementing this yet
-	 * For assignment 3.2: This function should set the distance and angle goals appropriately for PID (hint: using the setGoal functions in pid.c)
-	 * and wait until the error becomes sufficiently small and persistent before exiting. This function should NOT exit before then.
-	 *
-	 * HINT: Use a while loop to wait until the turn is complete
-	 *
-	 * You should also call resetPID before exiting this function so your rat is ready for the next instruction.
-	 */
 	setPIDGoalD(0);
 	setPIDGoalA(n*right_angle);
     while (1)
